5d0670986d0d75d31a790f4fca9c3e2c0bae6655
martin <martin@ryberg.org>
2021-02-19 11:28:42 +0100

    Add navbar script

---
+++ navbar.js
@@ -0,0 +1,3 @@
+function write_navbar() {
+    document.getElementById("navbar").innerHTML = '<nav><a href="index.html">Home</a> | <a href="people.html">People</a> | <a href="useful_links.html">Useful links</a></nav><hr>';
+}
